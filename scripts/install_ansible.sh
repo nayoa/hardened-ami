e4a014377243d5d8a8b3b96b00b15ef76b051d3a
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-06-14 12:03:19 +0100

    PTS-2636: Replace apt-get with apt

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Update the OS to begin with to catch up on the latest packages.
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 
 # Install necessary packages
-sudo apt-get install software-properties-common -y
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install ansible -y
+sudo apt install ansible -y
