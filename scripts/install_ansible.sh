55ddbc2b72dd5507686ec1ef54c19159dc5a8f25
Nayo Akinyele <nayoa@hotmail.co.uk>
2019-06-14 14:23:24 +0100

    PTS-2636: Fix bash script

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -4,13 +4,15 @@ set -e
 set -x
 
 # Non-interactive mode
+sudo -s
 export DEBIAN_FRONTEND=noninteractive
+exit
 
 # Update the OS to begin with to catch up on the latest packages.
 sudo apt-get -y update
-sudo -E apt-get -y upgrade
+sudo apt-get -y upgrade
 
 # Install necessary packages
 sudo apt-get -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo -E apt-get -y install ansible
+sudo apt-get -y install ansible
